91fda9049f3cab4ad61e6ea2eb083d9bd6d72d70
vinohts <vino.hts@gmail.com>
2025-12-14 09:59:57 +0530

    Add manual CDK deployment workflow via OIDC

--- automation/webserver/bin/webserver.ts
+++ automation/webserver/bin/webserver.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { WebserverStack } from '../lib/webserver-stack';
+import { PrivateWebserverStack } from '../lib/webserver-stack';
 
 const app = new cdk.App();
 
-new WebserverStack(app, 'PrivateWebserverStack', {
+new PrivateWebserverStack(app, 'PrivateWebserverStack', {
   env: {
-    account: '758890598841',
-    region: 'ap-south-1',
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
   },
 });
